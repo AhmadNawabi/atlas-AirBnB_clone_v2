4bd599350c1b8f3f19b83023a552b3e80235eb34
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-03-07 16:59:23 -0500

    update the 1-hbnb_route.py file and push them to the repo

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+
 """import flask from python librarry"""
 from flask import Flask
 """store flask into app variable"""
 app = Flask(__name__)
-"""Define a route for the root URL
-("/") with strict_slashes=false"""
 
 
 @app.route("/",  strict_slashes=False)
 def hello():
+    """Define a route for the root URL
+    ("/") with strict_slashes=false"""
     return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Define a route for the root URL
+    ("/HBNB") with strict_slashes=false"""
+    return "HBNB"
 
 
 if __name__ == "__main__":
