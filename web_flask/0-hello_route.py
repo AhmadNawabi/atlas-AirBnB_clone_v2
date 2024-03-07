ab61d1e29ed5d511742cb96b62c33080cc4ed5a8
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-03-07 11:04:58 -0500

    correct the pystyle for my file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+
 """import flask from python library"""
 from flask import Flask
 
 """store flask in the app variable"""
 app = Flask(__name__)
 
-"""Define a route for the root URL ("/") with strict_slashes=false"""
+"""Define a route for the root URL
+("/") with strict_slashes=false"""
 @app.route('/')
 def hello():
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    """Run the flask app on 0.0.0.0 and port 5000"""
+    """Run the flask app on 0.0.0.0
+    and port 5000"""
     app.run(host='0.0.0.0', port=5000)
