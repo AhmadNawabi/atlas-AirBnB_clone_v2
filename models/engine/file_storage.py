0f7fe2f6a26b4b31cb2c9ee4d4156ca8d9ae72d3
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-03-11 11:57:53 -0400

    update PEP8 style for the file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the FileStorage class
-"""
-
+"""Contains the FileStorage class"""
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -18,7 +15,6 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 
 class FileStorage:
     """serializes instances to a JSON file & deserializes back to instances"""
-
     # string - path to the JSON file
     __file_path = "file.json"
     # dictionary - empty but will store all objects by <class name>.id
@@ -37,7 +33,7 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
+            key = f"{obj.__class__.__name__}.{obj.id}"
             self.__objects[key] = obj
 
     def save(self):
@@ -55,16 +51,15 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = f"{obj.__class__.__name__}.{obj.id}"
             if key in self.__objects:
                 del self.__objects[key]
-
 
     def close(self):
         """Calls reload() method for deserializing the JSON file to objects"""
